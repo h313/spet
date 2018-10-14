f3bfe807705ce20d8bc5e4ec245ea6efa9856bb2
Harry <harry@h313.info>
2018-10-14 00:21:39 -0700

    add first 4 functions

--- parser.py
+++ parser.py
@@ -1,5 +1,23 @@
 index = 0
+location = 0
 data = [0 for i in range(40000)]
 
 def parse(inputs):
-	print(2)
+	while(index < len(inputs)):
+		# Print character at location
+		if inputs[index]['name'] == 'Dream Lantern':
+			print(chr(location))
+		# Input characters
+		elif inputs[index]['artist'] == 'Coldplay':
+			inp = input()
+			max = len(inputs[index]['artist'].split()[0]) + index
+			for i in range(index, max):
+				data[index] = ord(inputs[index]['artist'].split()[0][i])
+				index += 1
+		# Move forward
+		elif inputs[index]['artist'] == 'Radiohead' :
+			index += 1
+		# Move backward
+		elif inputs[index]['artist'] == "Green Day":
+			index -= 1
+		index++;
